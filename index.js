fda70c4dc1a0c0fff4ecb74bc3608c9d69168aab
aviranyosefi <aviran5600@gmail.com>
2023-01-15 14:28:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    const ipAddress = req.socket.remoteAddress;
+    const ipAddress =req.headers['x-forwarded-for'] 
     res.send(ipAddress);
 })
 
